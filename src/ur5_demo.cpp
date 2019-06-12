9efe0dc542305b099126c9f42463572c3037464a
Mike Lautman <mikeblautman@gmail.com>
2019-06-11 23:08:27 -0600

    tmp storage

--- src/ur5_demo.cpp
+++ src/ur5_demo.cpp
@@ -43,24 +43,28 @@ public:
   {
     refreshRobot();
 
-    // planning_scene_monitor_.reset(
-        // new planning_scene_monitor::PlanningSceneMonitor(planning_scene_, robot_model_loader_));
+    planning_scene_monitor_.reset(
+       new planning_scene_monitor::PlanningSceneMonitor(planning_scene_, robot_model_loader_));
 
-    // if (planning_scene_monitor_->getPlanningScene())
-    // {
-      // planning_scene_monitor->startSceneMonitor();
-      // planning_scene_monitor->startWorldGeometryMonitor();
-      // planning_scene_monitor->startStateMonitor();
-      // planning_scene_monitor_->setPlanningScenePublishingFrequency(100);
-      // planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE, "/planning_scene");
-      // planning_scene_monitor_->startSceneMonitor("/planning_scene");
-    // }
+    planning_scene_monitor_->setPlanningScenePublishingFrequency(100);
+    planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE);
+    planning_scene_monitor_->startStateMonitor();
+    planning_scene_monitor_->startSceneMonitor("/planning_scene");
 
     // Spin while we wait for the full robot state to become available
-    // while (!planning_scene_monitor_->getStateMonitor()->haveCompleteState() && ros::ok())
-    // {
-    //  ROS_INFO_STREAM_THROTTLE_NAMED(1, "UR5Demo", "Waiting for complete state from topic ");
-    // }
+    std::vector<std::string> missing_joints;
+    while (!planning_scene_monitor_->getStateMonitor()->haveCompleteState(missing_joints) && ros::ok())
+    {
+      std::stringstream ss;
+      ss << "Waiting for complete state from topic. Missing[";
+      for (auto &joint : missing_joints)
+        ss << joint << ", ";
+      ss << "]" << std::endl;
+      ROS_ERROR_STREAM_THROTTLE_NAMED(1, "UR5Demo", ss.str());
+    }
+
+    trajectory_execution_manager_.reset(new trajectory_execution_manager::TrajectoryExecutionManager(
+        planning_scene_monitor_->getRobotModel(), planning_scene_monitor_->getStateMonitor()));
 
     // online_visual_tools_->setPlanningSceneMonitor(planning_scene_monitor_);
     online_visual_tools_->setPlanningSceneTopic("/planning_scene");
@@ -71,32 +75,79 @@ public:
     online_visual_tools_->trigger();
   }
 
-  bool planTrajectory()
-  {
-    trajectory_name_ = "traj1";
-    const Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
-    const moveit_simple::InterpolationType cart = moveit_simple::interpolation_type::CARTESIAN;
-    const moveit_simple::InterpolationType joint = moveit_simple::interpolation_type::JOINT;
+  // bool planTrajectory()
+  // {
+  //   trajectory_name_ = "traj1";
+  //   const Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
+  //   const moveit_simple::InterpolationType cart = moveit_simple::interpolation_type::CARTESIAN;
+  //   const moveit_simple::InterpolationType joint = moveit_simple::interpolation_type::JOINT;
 
-    addTrajPoint(trajectory_name_, "home",      0.0, joint);
-    addTrajPoint(trajectory_name_, "tf_pub1",   0.0, joint, 8);
-    addTrajPoint(trajectory_name_, "tf_pub2",   12.0, joint, 8);
-    ROS_INFO_NAMED("UR5Demo", "plan finished");
-    return true;
-  }
+  //   addTrajPoint(trajectory_name_, "home",      0.0, joint);
+  //   addTrajPoint(trajectory_name_, "tf_pub1",   0.0, joint, 8);
+  //   addTrajPoint(trajectory_name_, "tf_pub2",   12.0, joint, 8);
+  //   ROS_INFO_NAMED("UR5Demo", "plan finished");
+  //   return true;
+  // }
 
-  bool executeTrajectory()
+  bool executeTrajectory(const std::string &trajectory_name, bool check_for_collisions)
   {
-    try
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "planning trajectory: " << trajectory_name);
+
+    std::vector<moveit_simple::JointTrajectoryPoint> traj = plan(trajectory_name, check_for_collisions);
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "plan finished, len " << traj.size());
+
+    moveit::core::RobotStatePtr virtual_robot_state(new moveit::core::RobotState(
+          planning_scene_monitor::LockedPlanningSceneRO(planning_scene_monitor_)->getCurrentState()));
+    virtual_robot_state->update();
+
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "got the current robot state");
+    // virtual_robot_state->printStatePositionsWithJointLimits(joint_group_);
+    robot_trajectory::RobotTrajectoryPtr trajectory(
+        new robot_trajectory::RobotTrajectory(robot_model_ptr_, joint_group_->getName()));
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "trajectory initialized ");
+
+    double dummy_dt = 0.01;  // this is overwritten and unimportant
+    trajectory->addPrefixWayPoint(virtual_robot_state, dummy_dt);
+
+    std::size_t count=0;
+    for (auto &point: traj)
     {
-      execute(trajectory_name_, true, true);
-      return true;
+      count++;
+      std::vector<double> target_point = point.jointPoint();
+      ROS_ERROR_STREAM_NAMED("UR5Demo",
+                             "point: " << count << "\t"
+                             << target_point[0]
+                             << target_point[1]
+                             << target_point[2]
+                             << target_point[3]
+                             << target_point[4]
+                             << target_point[5]);
+
+      virtual_robot_state->setJointGroupPositions(joint_group_, target_point);
+      virtual_robot_state->update();
+      trajectory->addSuffixWayPoint(virtual_robot_state, dummy_dt);
     }
-    catch (const std::exception& e)
-    {
-      ROS_ERROR_STREAM("Execution failed: " << e.what());
-      return false;
-    }
+
+    iterative_smoother_.computeTimeStamps(*trajectory, 1.0);
+
+    moveit_msgs::RobotTrajectory trajectory_msg;
+    trajectory->getRobotTrajectoryMsg(trajectory_msg);
+    ROS_DEBUG_STREAM_NAMED("UR5Demo", "msg \n" << trajectory_msg);
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "publishing trajectory to mvt");
+
+    visual_tools_->publishTrajectoryPath(trajectory_msg, virtual_robot_state, true);
+    visual_tools_->trigger();
+    visual_tools_->prompt("next");
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "calling execute");
+
+    trajectory_execution_manager_->clear();
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "cleared");
+    trajectory_execution_manager_->push(trajectory_msg);
+    ROS_ERROR_STREAM_NAMED("UR5Demo", "pushed traj \n" << trajectory_msg);
+    trajectory_execution_manager_->execute();
+    ROS_ERROR_NAMED("UR5Demo", "execute");
+    return trajectory_execution_manager_->waitForExecution();
+
   }
 
   bool spawnObject(const geometry_msgs::Pose& pose)
@@ -219,9 +270,18 @@ public:
 
   std::string trajectory_name_;
 
+  // Trajectory execution
+  trajectory_execution_manager::TrajectoryExecutionManagerPtr trajectory_execution_manager_;
+  trajectory_processing::IterativeSplineParameterization iterative_smoother_;
+
+  // PSM
+
   // For visualizing things in rviz
   moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
-  // planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_;
+
+  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_;
+
+
 };
 
 
