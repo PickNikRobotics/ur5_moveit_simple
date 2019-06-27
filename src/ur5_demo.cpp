a041a1bbe019515446894a0a545a80ade4207210
Mike Lautman <mikeblautman@gmail.com>
2019-06-27 17:24:41 -0600

    Retime traj vel scaling (#5)
    * Use OnlineRobot and fix launch bringup
    * Implement simple pick/place demo
    * adding robotiq control and improving demo
    * using velocity and acceleration scaling when retiming the trajectory
    * adding moveit_simple to rosinstall

--- src/ur5_demo.cpp
+++ src/ur5_demo.cpp
@@ -22,29 +22,56 @@
 #include <moveit/trajectory_execution_manager/trajectory_execution_manager.h>
 #include <moveit/trajectory_processing/iterative_spline_parameterization.h>
 
+// Robotiq
+#include <robotiq_2f_gripper_control/Robotiq2FGripper_robot_output.h>
+
 // Visual tools
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 #include <moveit_simple/moveit_simple.h>
 #include <moveit_simple/prettyprint.hpp>
 
+#include <Eigen/Geometry>
+#include <eigen_conversions/eigen_msg.h>
+#include <tf/transform_datatypes.h>
+
 namespace moveit_simple
 {
-
-class UR5Demo : public Robot
+class UR5Demo : public OnlineRobot
 {
 public:
-  UR5Demo()
-  : Robot(ros::NodeHandle(), "robot_description", "arm", "base_link", "tool_custom")
+  UR5Demo(ros::NodeHandle nh) : OnlineRobot(nh, "robot_description", "arm", "base_link", "tool0"), nh_(nh)
   {
     refreshRobot();
+
+    robotiq_publisher_ =
+        nh_.advertise<robotiq_2f_gripper_control::Robotiq2FGripper_robot_output>("Robotiq2FGripperRobotOutput", 10);
+    if (false)
+      loadPlanningSceneMonitor();
+
+    online_visual_tools_->setPlanningSceneTopic("/planning_scene");
+    online_visual_tools_->loadMarkerPub();
+    online_visual_tools_->loadTrajectoryPub("/display_planned_path");
+    online_visual_tools_->deleteAllMarkers();  // clear all old markers
+    online_visual_tools_->enableBatchPublishing();
+    online_visual_tools_->trigger();
+  }
+
+  bool loadPlanningSceneMonitor()
+  {
     planning_scene_monitor_.reset(
         new planning_scene_monitor::PlanningSceneMonitor(planning_scene_, robot_model_loader_));
 
-    planning_scene_monitor_->setPlanningScenePublishingFrequency(100);
-    planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE);
-    planning_scene_monitor_->startStateMonitor();
-    planning_scene_monitor_->startSceneMonitor("/planning_scene");
+    if (planning_scene_monitor_->getPlanningScene())
+    {
+      planning_scene_monitor_->startSceneMonitor();
+      planning_scene_monitor_->startWorldGeometryMonitor();
+      planning_scene_monitor_->startStateMonitor();
+      planning_scene_monitor_->setPlanningScenePublishingFrequency(100);
+      planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE,
+                                                            "/planning_scene");
+      planning_scene_monitor_->startSceneMonitor("/planning_scene");
+    }
 
     // Spin while we wait for the full robot state to become available
     while (!planning_scene_monitor_->getStateMonitor()->haveCompleteState() && ros::ok())
@@ -52,102 +79,182 @@ public:
       ROS_INFO_STREAM_THROTTLE_NAMED(1, "UR5Demo", "Waiting for complete state from topic ");
     }
 
-    trajectory_execution_manager_.reset(new trajectory_execution_manager::TrajectoryExecutionManager(
-        planning_scene_monitor_->getRobotModel(), planning_scene_monitor_->getStateMonitor()));
-
-    visual_tools_.reset(new moveit_visual_tools::MoveItVisualTools("/world", "/moveit_visual_marker", planning_scene_monitor_));
-    visual_tools_->setPlanningSceneTopic("/planning_scene");
-    visual_tools_->loadMarkerPub();
-    visual_tools_->loadTrajectoryPub("/robot_traj_display");
-    visual_tools_->deleteAllMarkers();  // clear all old markers
-    visual_tools_->enableBatchPublishing();
-    visual_tools_->hideRobot();         // show that things have been reset
-    visual_tools_->trigger();
-
+    online_visual_tools_->setPlanningSceneMonitor(planning_scene_monitor_);
   }
 
-  bool planTrajectory()
+  bool spawnObject(const geometry_msgs::Pose& pose)
   {
-    trajectory_name_ = "traj1";
-    const Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
-    const moveit_simple::InterpolationType cart = moveit_simple::interpolation_type::CARTESIAN;
-    const moveit_simple::InterpolationType joint = moveit_simple::interpolation_type::JOINT;
-
-    addTrajPoint(trajectory_name_, "home",      4.0, joint);
-    addTrajPoint(trajectory_name_, "tf_pub1",   9.0, joint, 8);
-    addTrajPoint(trajectory_name_, "tf_pub2",   12.0, joint, 8);
-    ROS_INFO_NAMED("UR5Demo", "plan finished");
+    online_visual_tools_->publishCollisionCuboid(pose, 0.04, 0.04, 0.15, "object", rviz_visual_tools::colors::GREEN);
+    online_visual_tools_->triggerPlanningSceneUpdate();
+    return true;
+  }
+  void setIKSeedMidPoint(const std::string& pose_id)
+  {
+    robot_state::RobotStatePtr virtual_state = online_visual_tools_->getSharedRobotState();
+    virtual_state->setToDefaultValues(joint_group_, pose_id);
+    virtual_state->update();
+    std::vector<double> mid_point;
+    virtual_state->copyJointGroupPositions(joint_group_, mid_point);
+    setIKSeedStateMidPoint(mid_point);
+  }
+  bool openGripper()
+  {
+    robotiq_2f_gripper_control::Robotiq2FGripper_robot_output open_msg;
+    open_msg.rACT = 1;
+    open_msg.rGTO = 1;
+    open_msg.rATR = 0;
+    open_msg.rPR = 0;
+    open_msg.rSP = 255;
+    open_msg.rFR = 175;
+    robotiq_publisher_.publish(open_msg);
+    ros::Duration(0.5).sleep();
+    return true;
+  }
+  bool closeGripper()
+  {
+    robotiq_2f_gripper_control::Robotiq2FGripper_robot_output close_msg;
+    close_msg.rACT = 1;
+    close_msg.rGTO = 1;
+    close_msg.rATR = 0;
+    close_msg.rPR = 255;
+    close_msg.rSP = 255;
+    close_msg.rFR = 175;
+    robotiq_publisher_.publish(close_msg);
+    ros::Duration(0.5).sleep();
     return true;
   }
 
-  bool executeTrajectory()
+  bool runPickAndPlace()
   {
-    ROS_INFO_NAMED("UR5Demo", "planning trajectory: " << trajectory_name_);
+    geometry_msgs::Pose object_pose;
+    object_pose.orientation.w = 1.0;
+    object_pose.position.y = 0.35;
+    object_pose.position.x = 0.2;
+    object_pose.position.z = 0.075;
+    spawnObject(object_pose);
 
-    std::vector<moveit_simple::JointTrajectoryPoint> traj = plan(trajectory_name_, false);
-    ROS_INFO_STREAM_NAMED("UR5Demo", "plan finished, len " << traj.size());
+    // create pick pose
+    double approach_distance = 0.075;
+    Eigen::Isometry3d pick_pose;
+    object_pose.position.z = 0.18 + approach_distance;
+    object_pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, M_PI, 0);
+    tf::poseMsgToEigen(object_pose, pick_pose);
 
-    moveit::core::RobotStatePtr virtual_robot_state(new moveit::core::RobotState(
-          planning_scene_monitor::LockedPlanningSceneRO(planning_scene_monitor_)->getCurrentState()));
-    virtual_robot_state->update();
-    ROS_INFO_STREAM_NAMED("UR5Demo", "got the current robot state");
+    // create place pose
+    Eigen::Isometry3d place_pose = pick_pose * Eigen::Translation3d(0.0, -0.70, 0);
+    place_pose = place_pose * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitZ());
+    std::vector<double> seed, pick_state, place_state;
 
-    robot_trajectory::RobotTrajectoryPtr trajectory(
-        new robot_trajectory::RobotTrajectory(robot_model_ptr_, joint_group_->getName()));
-    ROS_INFO_STREAM_NAMED("UR5Demo", "trajectory initialized ");
+    // Use ready as seed
+    robot_state::RobotStatePtr virtual_state = online_visual_tools_->getSharedRobotState();
+    virtual_state->setToDefaultValues(joint_group_, "ready");
+    virtual_state->update();
+    virtual_state->copyJointGroupPositions(joint_group_, seed);
 
-    double dummy_dt = 0.01;  // this is overwritten and unimportant
-    trajectory->addPrefixWayPoint(virtual_robot_state, dummy_dt);
+    double velocity_scaling = 0.6;
+    double acceleration_scaling = 0.6;
+    setMaxVelocityScalingFactor(velocity_scaling);
+    setMaxAccelerationScalingFactor(acceleration_scaling);
 
-    std::size_t count=0;
-    for (auto &point: traj)
+    try
     {
-      count++;
-      std::vector<double> target_point = point.jointPoint();
-      ROS_INFO_STREAM_NAMED("UR5Demo", "point: " << count << "\t" << target_point[0] << target_point[1] << target_point[2] << target_point[3] << target_point[4] << target_point[5]);
+      if (getPickPlaceJointSolutions(pick_pose, place_pose, "tool0", 1.0, seed, pick_state, place_state))
+      {
+        // compute actual pick/place points
+        Eigen::Isometry3d pick_down_pose, place_down_pose;
+        getPose(pick_state, pick_down_pose);
+        pick_down_pose *= Eigen::Translation3d(0, 0, approach_distance);
+        getPose(place_state, place_down_pose);
+        place_down_pose *= Eigen::Translation3d(0, 0, approach_distance);
 
-      virtual_robot_state->setJointGroupPositions(joint_group_, target_point);
-      virtual_robot_state->update();
-      trajectory->addSuffixWayPoint(virtual_robot_state, dummy_dt);
+        const moveit_simple::InterpolationType cart = moveit_simple::interpolation_type::CARTESIAN;
+        const moveit_simple::InterpolationType joint = moveit_simple::interpolation_type::JOINT;
+        std::unique_ptr<moveit_simple::TrajectoryPoint> pick_point = std::unique_ptr<moveit_simple::TrajectoryPoint>(
+            new moveit_simple::JointTrajectoryPoint(pick_state, 0.0, "pick_point"));
+        std::unique_ptr<moveit_simple::TrajectoryPoint> pick_point2 = std::unique_ptr<moveit_simple::TrajectoryPoint>(
+            new moveit_simple::JointTrajectoryPoint(pick_state, 0.0, "pick_point"));
+        std::unique_ptr<moveit_simple::TrajectoryPoint> pick_down_point =
+            std::unique_ptr<moveit_simple::TrajectoryPoint>(
+                new moveit_simple::CartTrajectoryPoint(pick_down_pose, 0.0, "pick_down_point"));
+
+        std::unique_ptr<moveit_simple::TrajectoryPoint> place_point = std::unique_ptr<moveit_simple::TrajectoryPoint>(
+            new moveit_simple::JointTrajectoryPoint(place_state, 0.0, "place_point"));
+        std::unique_ptr<moveit_simple::TrajectoryPoint> place_point2 = std::unique_ptr<moveit_simple::TrajectoryPoint>(
+            new moveit_simple::JointTrajectoryPoint(place_state, 0.0, "place_point"));
+        std::unique_ptr<moveit_simple::TrajectoryPoint> place_down_point =
+            std::unique_ptr<moveit_simple::TrajectoryPoint>(
+                new moveit_simple::CartTrajectoryPoint(place_down_pose, 0.0, "place_down_point"));
+
+        // TODO(henningkayser): move to pre-pose that doesn't collide when interpolating to pick_point
+        openGripper();
+        bool check_collisions = false;
+        std::string traj_name = "home";
+        clearTrajectory(traj_name);
+        addTrajPoint(traj_name, "ready", 3, joint, 100);
+        ROS_ERROR_STREAM("press 'next' in RvizVisaualToolsGui to execute 'home'");
+        online_visual_tools_->prompt("home");
+        execute(traj_name, check_collisions, true);
+        openGripper();
+
+        traj_name = "pick";
+        clearTrajectory(traj_name);
+        addTrajPoint(traj_name, pick_point, joint, 100);
+        addTrajPoint(traj_name, pick_down_point, cart, 10);
+        ROS_ERROR_STREAM("press 'next' in RvizVisaualToolsGui to execute 'pick'");
+        online_visual_tools_->prompt("pick");
+        execute(traj_name, check_collisions, true);
+
+        // pick object
+        online_visual_tools_->attachCO("object", "tool0");
+        online_visual_tools_->trigger();
+        closeGripper();
+
+        traj_name = "pick_retreat";
+        clearTrajectory(traj_name);
+        addTrajPoint(traj_name, pick_point2, cart, 10);
+        execute(traj_name, check_collisions, true);
+
+        // lift, move, lower
+        traj_name = "place";
+        clearTrajectory(traj_name);
+        addTrajPoint(traj_name, place_point, cart, 50);
+        addTrajPoint(traj_name, place_down_point, cart, 10);
+        ROS_ERROR_STREAM("press 'next' in RvizVisaualToolsGui to execute 'place'");
+        online_visual_tools_->prompt("place");
+        execute(traj_name, check_collisions, true);
+        openGripper();
+
+        // drop object
+        online_visual_tools_->cleanupACO("object");
+        online_visual_tools_->trigger();
+
+        // retreat
+        traj_name = "place_retreat";
+        clearTrajectory(traj_name);
+        addTrajPoint(traj_name, place_point2, cart, 50);
+        execute(traj_name, check_collisions, true);
+      }
     }
-
-    iterative_smoother_.computeTimeStamps(*trajectory, 1.0);
-
-    moveit_msgs::RobotTrajectory trajectory_msg;
-    trajectory->getRobotTrajectoryMsg(trajectory_msg);
-    ROS_DEBUG_STREAM_NAMED("UR5Demo", "msg \n" << trajectory_msg);
-    ROS_INFO_STREAM_NAMED("UR5Demo", "publishing trajectory to mvt");
-
-    visual_tools_->publishTrajectoryPath(trajectory_msg, virtual_robot_state, true);
-    visual_tools_->prompt("next");
-    ROS_INFO_STREAM_NAMED("UR5Demo", "calling execute");
-
-    trajectory_execution_manager_->clear();
-
-    ROS_INFO_STREAM_NAMED("UR5Demo", "cleared");
-    trajectory_execution_manager_->push(trajectory_msg);
-    ROS_INFO_STREAM_NAMED("UR5Demo", "pushed traj");
-    trajectory_execution_manager_->execute();
-    ROS_INFO_NAMED("UR5Demo", "execute");
-
-    return trajectory_execution_manager_->waitForExecution();
+    catch (const std::exception& e)
+    {
+      ROS_ERROR_STREAM("Execution failed: " << e.what());
+      return false;
+    }
+    return true;
   }
 
   std::string trajectory_name_;
 
-  // Trajectory execution
-  trajectory_execution_manager::TrajectoryExecutionManagerPtr trajectory_execution_manager_;
-  trajectory_processing::IterativeSplineParameterization iterative_smoother_;
-
-  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_;
-
   // For visualizing things in rviz
   moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
+  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_;
 
+  ros::NodeHandle nh_;
+  ros::Publisher robotiq_publisher_;
 };
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   std::string name = "moveit_simple";
   ros::init(argc, argv, name);
@@ -155,13 +262,34 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(4);
   spinner.start();
 
-  std::unique_ptr<moveit_simple::UR5Demo> demo(new moveit_simple::UR5Demo());
+  std::unique_ptr<moveit_simple::UR5Demo> demo(new moveit_simple::UR5Demo(ros::NodeHandle()));
   ROS_INFO("Initialized");
 
-  demo->planTrajectory();
-  demo->executeTrajectory();
+  // limit joint windup
+  // TODO(henningkayser): add suitable waypoints to showcase this feature
+  demo->setLimitJointWindup(true);
+  std::map<size_t, double> seed_fractions;
+  seed_fractions[0] = 0.5;  // limit base windup
+  seed_fractions[1] = 0.5;  // limit shoulder windup
+  seed_fractions[2] = 0.5;  // limit elbow windup
+  seed_fractions[3] = 0.5;  // limit wrist 1 joint windup
+  seed_fractions[4] = 0.5;  // limit wrist 2 joint windup
+  seed_fractions[5] = 0.5;  // limit wrist 3 joint windup
+  demo->setIKSeedStateFractions(seed_fractions);
 
-  ros::waitForShutdown();
+  demo->setIKSeedMidPoint("ready");
+  // define end effector symmetry type and axis
+  demo->setEndEffectorSymmetry(moveit_simple::EndEffectorSymmetry::Rectangular, Eigen::Vector3d::UnitZ());
+
+  // run simple pick and place
+  while (ros::ok())
+    demo->runPickAndPlace();
+
+  // plan and execute waypoint trajectory
+  // demo->planTrajectory();
+  // demo->executeTrajectory();
+
+  // ros::waitForShutdown();
+  ros::shutdown();
   return 0;
-
 }
